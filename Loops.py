2e3f0af5e6f893a5e06bf277e855dcaab686dfb9
moath alosaimi <moathalosimie@hotmail.com>
2022-10-11 19:48:40 +0300

    i solved the Loop Lab

---
+++ Loops.py
@@ -0,0 +1,30 @@
+from cgi import print_arguments
+
+
+num : int = 210
+
+for num in range(45,210) :
+    if num == 100 :
+        continue
+
+    print(num)
+    if num == 205:
+        break
+
+
+#answer : int = 168
+user_input : str = ""
+
+while not user_input == 168  :
+    user_input = int(input("what is the product of 7 * 24 ?"))
+    if user_input == 168:
+        print("You answered this Question correctly")
+    else:
+        
+        print("Your Answer is wrong try again..", user_input)
+
+        
+
+    
+    
+
