82cedb2a66bc15c490933d6913b204098c917fe6
bahiyyah_4 <bahiab144@gmail.com>
2022-10-11 19:48:55 +0300

    add lab 3

---
+++ lap_loops.py
@@ -0,0 +1,24 @@
+
+# 1) Using range(),
+#  make a range from 45 to 210, using a for loop iterate
+# over the sequence and print the elements. Skip the number 100 and break the loop at 205
+for i in range(45, 210):
+    if i == 100:
+        continue
+    elif i == 205:
+        break
+    else:
+        print(i)
+
+
+# 2
+
+
+answer = int(input("what is the product of 7 * 24 ?"))
+while True:
+    if answer == 168:
+        print("You answered this Question correctly")
+        break
+    else:
+        print("Your Answer is wrong try again..")
+        answer = int(input("what is the product of 7 * 24 ?"))
