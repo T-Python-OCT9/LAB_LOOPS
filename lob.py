1077a34ddf8034b23c712c842ee12f12755e8ef4
bakhet <baketalrwes@gmail.com>
2022-10-11 20:13:19 +0300

    loop

---
+++ lob.py
@@ -0,0 +1,18 @@
+for i in range(45, 210):
+    if i == 100:
+        continue
+    if i == 205:
+        break
+    print(i)
+
+
+
+while True:
+    total = int(input("what is the product of 7 * 24 ?"))
+        
+
+    if total == 7*24:
+        print("You answered this Question correctly" )
+        break
+    else:
+        print("Your Answer is wrong try again.." )
