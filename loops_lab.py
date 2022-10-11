4779e7e5b89aadbf58a6dac1dd12d98c6431657f
saud <Saudalotibi79@gmail.com>
2022-10-11 20:12:43 +0300

    better

--- loops_lab.py
+++ loops_lab.py
@@ -10,8 +10,8 @@ for numbers in range(45, 210):
 
 # solve the second part of the qustion
 while True:
-    user_input = input("what is the product of 7 * 24 ? ")
-    if user_input == '168':
+    user_input = int(input("what is the product of 7 * 24 ? "))
+    if user_input == 7*24:
         print("You answered this Question correctly")
         break 
     else:
