66f517c916c287cc02fcaa7f8191ee1de1397e5d
Mohammed.I.Alharbi <name@gmail.com>
2022-10-11 19:34:12 +0300

    lab3 has been solved :)

---
+++ lab3.py
@@ -0,0 +1,18 @@
+
+for i in range(45,210):
+    if i == 100:
+        continue
+    elif i == 205:
+        break
+    print(i)
+
+x = 1
+while x == 1:
+    total = input("what is the product of 7 * 24 ?")
+    if total == '168':
+        print("You answered this Question correctly")
+        break
+    else:
+        print("Your Answer is wrong try again..")
+
+    
