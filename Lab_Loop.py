be6d5ae712680d3b096633dbff7e6e85063805f2
Roaa Albishri <roaa20101@hotmail.com>
2022-10-11 19:28:14 +0300

    Solved Lab_Loop

---
+++ Lab_Loop.py
@@ -0,0 +1,23 @@
+# LAB_LOOPS
+
+## 1) Using range(),  make a range from 45 to 210, using a for loop iterate over the sequence and print the elements. Skip the number 100 and break the loop at 205
+for x in range(45, 210):
+    if x == 100 :
+        continue
+    if x == 205 :
+        break
+    print("Elements : ", x)
+
+
+## 2) Using a while loop and input , do the following :
+#Ask the the user : "what is the product of 7 * 24 ?"
+user_input = input("what is the product of 7 * 24 ?")
+#check if the answer is right then exit the loop and print "You answered this Question correctly"
+while user_input != "168" :
+    print("Your Answer is wrong try again..")
+    user_input = input("what is the product of 7 * 24 ?")
+    continue
+# if the answer is wrong, then print "Your Answer is wrong try again.." and show the user the question again.
+else:
+        print("You answered this Question correctly")
+    
