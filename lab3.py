7e7fd9eec2bc8b0f0f2004f55af63322e0f6cbf0
Mohammed.I.Alharbi <name@gmail.com>
2022-10-11 19:39:25 +0300

    Another modify

--- lab3.py
+++ lab3.py
@@ -8,11 +8,10 @@ for i in range(45,210):
 
 x = 1
 while x == 1:
-    total = input("what is the product of 7 * 24 ?")
-    if total == '168':
+    total = int(input("what is the product of 7 * 24 ?"))
+    
+    if total == 168:
         print("You answered this Question correctly")
         break
     else:
         print("Your Answer is wrong try again..")
-
-    
